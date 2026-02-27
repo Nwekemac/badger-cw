72914d150d51c34c11754a0b0b0c1c433d306dc3
Machanus Nweke <19354491@brookes.ac.uk>
2026-02-27 19:44:41 +0000

    Created and Tested the display_staff function which displays a single staff record. Current Salary and Years of Employment Calaculatons working well on the fly without being stored in memory

--- badger-cw1.asm
+++ badger-cw1.asm
@@ -32,6 +32,14 @@ section .data
     lbl_salary:     db "Current Salary: £", 0
     lbl_email:      db "Email: ", 0
     lbl_staff_id:   db "Staff ID: ", 0
+    lbl_current_salary: db "Current Salary: £", 0
+    lbl_department:  db "Department: ", 0
+    lbl_staff_years: db "Years of employment: ", 0
+        
+        ;Department Strings
+        dept_1: db "Zoo Keeper", 0
+        dept_2: db "Shop", 0
+        dept_3: db "Cafe", 0
     
                
     msg_add_staff: db "You can now add a staff!", 0
@@ -283,10 +291,112 @@ add_staff:
 
         
 ;DISPLAY_STAFF FUNCTION TO DISPLAY A SINGLE STAFF RECORD
+;Take an argument in RDI that is equal to the base address of that particular staff record
 display_staff:
     push rbp
     mov rbp, rsp
     sub rsp, 32
+    
+    push rbx
+    push rcx
+    push rdx
+    push rdi
+    push rsi
+    
+    mov rbx, rdi ; RBX now contains the the base address of the individual record
+    
+    ;Print Surname
+    mov rdi, lbl_surname
+    call print_string_new
+    lea rdi, [rbx + staff_surname_pfb]
+    call print_string_new
+    call print_nl_new
+    
+    ;Print Firstname
+    mov rdi, lbl_firstname
+    call print_string_new
+    lea rdi, [rbx + staff_firstname_pfb]
+    call print_string_new
+    call print_nl_new
+    
+    ;Print Staff ID
+    mov rdi, lbl_staff_id
+    call print_string_new
+    lea rdi, [rbx + staff_id_pfb]
+    call print_string_new
+    call print_nl_new
+    
+    ;Print Staff DEPARTMENT
+    mov rdi, lbl_department
+    call print_string_new
+    movzx r9, BYTE[rbx + staff_dept_pfb] ;SAVE the value of department (1, 2 or 3) to 69
+    ;Print the deparment based on the integer stored in r9
+    cmp r9, 1
+    je print_dept_1
+    cmp r9, 2
+    je print_dept_2
+    cmp r9, 3
+    je print_dept_3
+    
+    
+    
+    print_dept_1:
+        mov rdi, dept_1
+        call print_string_new
+        call print_nl_new
+        jmp print_years
+    print_dept_2:
+        mov rdi, dept_2
+        call print_string_new
+        call print_nl_new
+        jmp print_years
+    print_dept_3:
+        mov rdi, dept_3
+        call print_string_new
+        call print_nl_new
+        jmp print_years
+        
+    print_years:
+    ;Print YEars of Experience
+    mov rdi, lbl_staff_years
+    call print_string_new
+    ; years of Experience = current_year - staff_year
+    movzx r10, WORD[rbx + staff_year_pfb]
+    movzx r11, WORD[current_year]
+    sub r11, r10 ;NOW R11 hold the value of years of experience
+    mov rdi, r11
+    call print_int_new
+    call print_nl_new
+    
+    ;Print ANNUAL SALARY
+    mov rdi, lbl_current_salary
+    call print_string_new
+    ;CALCULATE CURRENT SALAry
+    ; current salary = starting salary + (200 * years of experience)
+    ;mov rcx, r11
+    imul r11, 200
+    movzx r12, WORD[rbx + staff_salary_pfb]
+    add r11, r12
+    mov rdi, r11
+    call print_int_new
+    call print_nl_new
+    
+    
+    ;Print EMAIL
+    mov rdi, lbl_email
+    call print_string_new
+    lea rdi, [rbx + staff_email_pfb]
+    call print_string_new
+    call print_nl_new
+    
+    
+    ;NOTE to SELF: pop and push all the registers that were used
+    pop rsi
+    pop rdi
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rbp
     
     add rsp, 32
     pop rbp
